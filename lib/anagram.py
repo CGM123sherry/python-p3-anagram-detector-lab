17ed2fc2c331758551ae7fb952cca2acc01d7ef2
CGM123sherry <g.christinemurithi@gmail.com>
2024-09-02 17:57:25 +0300

    complete anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        # Store the original word and its sorted version
+        self.word = word
+        self.sorted_word = sorted(word)
+
+    def match(self, word_list):
+        # Return a list of words from word_list that are anagrams of self.word
+        return [word for word in word_list if sorted(word) == self.sorted_word]
